b573b2c9a36663b9f9cdcffd4623c2cc2888a4f7
tshepomcode <tshepomcode@gmail.com>
2020-05-15 16:35:04 +0200

    Corrected number check

--- fibonacci.js
+++ fibonacci.js
@@ -5,15 +5,13 @@ const fibonacci = function (n) {
 
     if (n < 0) {
         return "OOPS";
-    } else if (isFinite(n)) {
-        if (n === 0) {
-            return 0;
-        } else if (n === 1) {
-            return 1;
-        } else {
-            total = fibonacci(n - 1) + fibonacci(n - 2);
-            return total;
-        }
+    } else if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    } else {
+        total = fibonacci(n - 1) + fibonacci(n - 2);
+        return total;
     }
 
 }
